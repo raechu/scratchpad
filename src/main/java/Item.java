b5d0640a668e42ed7c324349a20b1a741559c4a6
Ryan Howard <ryan.howard@shinesolutions.com>
2018-08-16 15:48:41 +1000

    added sale calculator and item

---
+++ src/main/java/Item.java
@@ -0,0 +1,17 @@
+package main.java;
+
+public class Item {
+    private int cost;
+
+    public Item(int cost) {
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,10 @@
 package main.java;
 
 public class Runner {
+
+    public static void main(String[] args) {
+        Item item = new Item(15);
+        SaleCalculator saleCalculator = new SaleCalculator();
+        System.out.println("Price: $" + saleCalculator.getCost(item, 20));
+    }
 }
